1ef9a24f2f7db3a003c49f269520f195234b4108
Alex Simion <simion.alexgabriel@yahoo.com>
2020-04-26 23:45:41 +0300

    Made it work

--- server.js
+++ server.js
@@ -1,53 +1,50 @@
-const express = require('express');
-const solc = require('solc');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const solc = require("solc");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 let app = express();
 app.use(cors());
 
-app.listen(3000);
+app.listen(3001);
 
-app.get('/', function(req, res) {
-    const contractPath = path.resolve(__dirname, 'contracts', 'FoodChain.sol');
-    let contractSource = fs.readFileSync(contractPath).toString('UTF-8');
+app.get("/", function(req, res) {
+  const contractPath = path.resolve(__dirname, "contracts", "FoodChain.sol");
+  let contractSource = fs.readFileSync(contractPath).toString("UTF-8");
 
-    var solcInput = {
-        language: "Solidity",
-        sources: { 
-            contract: {
-                content: contractSource
-            }
-         },
-        settings: {
-            optimizer: {
-                enabled: true
-            },
-            evmVersion: "byzantium",
-            outputSelection: {
-                "*": {
-                  "": [
-                    "legacyAST",
-                    "ast"
-                  ],
-                  "*": [
-                    "abi",
-                    "evm.bytecode.object",
-                    "evm.bytecode.sourceMap",
-                    "evm.deployedBytecode.object",
-                    "evm.deployedBytecode.sourceMap",
-                    "evm.gasEstimates"
-                  ]
-                },
-            }
+  var solcInput = {
+    language: "Solidity",
+    sources: {
+      contract: {
+        content: contractSource
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true
+      },
+      evmVersion: "byzantium",
+      outputSelection: {
+        "*": {
+          "": ["legacyAST", "ast"],
+          "*": [
+            "abi",
+            "evm.bytecode.object",
+            "evm.bytecode.sourceMap",
+            "evm.deployedBytecode.object",
+            "evm.deployedBytecode.sourceMap",
+            "evm.gasEstimates"
+          ]
         }
-    };
-    
-    solcInput = JSON.stringify(solcInput);
-    let contractObject = solc.compile(solcInput);
-    contractObject = JSON.parse(contractObject);
-    
-    res.status(200);
-    res.send(contractObject);
+      }
+    }
+  };
+
+  solcInput = JSON.stringify(solcInput);
+  let contractObject = solc.compile(solcInput);
+  contractObject = JSON.parse(contractObject);
+
+  res.status(200);
+  res.send(contractObject);
 });
